e7e7ea69d4651f48bc4d8442beaa185e2bdacb39
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-03-29 23:38:44 +0700

    dapet nilai 100

--- main.go
+++ main.go
@@ -3,10 +3,26 @@ package main
 import "fmt"
 
 func TicketPlayground(height, age int) int {
-	return 0 // TODO: replace this
+	var hargaBayar int
+
+	if (age > 12) {
+		hargaBayar = 100000
+	} else if (age >= 12) || height > 160 {
+		hargaBayar = 60000
+	} else if (age >= 10 && age <= 11) || height > 150 {
+		hargaBayar = 40000
+	} else if (age >= 8 && age <= 9) || height > 135 {
+		hargaBayar = 25000
+	} else if (age >= 5 && age <= 7) || height > 120 {
+		hargaBayar = 15000
+	} else if age < 5 {
+		hargaBayar = -1
+	}
+
+	return hargaBayar // TODO: replace this
 }
 
 // gunakan untuk melakukan debug
 func main() {
-	fmt.Println(TicketPlayground(160, 11))
+	fmt.Println(TicketPlayground(120, 10))
 }
